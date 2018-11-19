4e08cc949b9b5cde0aaf69cacaca99cb3ce33ebb
Michal <jaasdqwe@gmail.com>
2018-11-19 22:30:19 +0900

    changed look a little, added missing link for password reset

--- pro_subjectrate/settings.py
+++ pro_subjectrate/settings.py
@@ -139,4 +139,10 @@ LOGOUT_REDIRECT_URL = 'index'
 # informing crispy forms that we use bootstrap 4
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
