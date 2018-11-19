79806e38bbb9b14ee417f2dbf5a38265a24b598f
Michal <jaasdqwe@gmail.com>
2018-11-19 21:28:35 +0900

    created models for storing subject reviews

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class CourseName(models.Model):
+    course_name = models.CharField(max_length=225)
+
+    def __str__(self):
+        return self.course_name
+
+class Professor(models.Model):
+    professor_name = models.CharField(max_length=255, default="")
+
+    def __str__(self):
+        return self.professor_name
+
+class Review(models.Model):
+
+    course_name = models.ForeignKey(
+        CourseName,
+        on_delete=models.CASCADE
+    )
+
+    professor_name = models.ForeignKey(
+        Professor,
+        on_delete=models.CASCADE
+    )
+
+    how_easy = models.IntegerField(
+        default=0,
+        validators=[MaxValueValidator(100), MinValueValidator(0)],
+    )
+
+    how_interesting = models.IntegerField(
+        default=0,
+        validators=[MaxValueValidator(100), MinValueValidator(0)],
+    )
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    review_body = models.TextField()
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review_body[:50]
+
+    def get_absolute_url(self):
+        return reverse('review-detail', args=[str(self.id)])
+
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
+
+
+# class ReviewListView(LoginRequiredMixin, ListView):
+
+
